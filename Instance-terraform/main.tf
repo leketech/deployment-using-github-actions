f9306913ef171018a94d0db1d3d6e4d023398da8
Aduraleke Faith Akintade <103920213+leketech@users.noreply.github.com>
2024-06-19 14:17:16 +0100

    Update main.tf

--- Instance-terraform/main.tf
+++ Instance-terraform/main.tf
@@ -31,9 +31,9 @@ resource "aws_security_group" "my-ec2" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-03f4878755434977f" # change your ami name 
+  ami                    = "ami-0e001c9271cf7f3b9" # change your ami name 
   instance_type          = "t2.medium"
-  key_name               = "my key"
+  key_name               = "few"
   vpc_security_group_ids = [aws_security_group.my-ec2.id]
   user_data              = templatefile("./script.sh", {})
 
